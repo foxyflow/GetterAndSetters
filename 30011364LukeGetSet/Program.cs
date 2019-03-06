5a48ec89a076d80107225731b1a9df02f364e3ed
Luke Fox <30011364@student.toiohomai.ac.nz>
2019-03-06 16:41:18 +1300

    unfinished

---
+++ 30011364LukeGetSet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30011364LukeGetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fdgfdgfdsgfds
+        }
+    }
+}
---
+++ GetSet2019/GetSet2019/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSet2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("name please");
+           public string name (Console.ReadLine());
+            Console.WriteLine("type in your credit card number");
+          private  int creditcard = Console.ReadLine();
+          private  int pin = Console.ReadLine();
+          private  int expiryDate = Console.ReadLine();
+        }
+    }
+class CreditCard //Attributes and Properties 
+{
+    public string cardName;
+    private int cardNo;
+    private int pinNo;
+    public int expiryDate;
+
+    public string Creditcard { get; set; }
+    public int CardNo {private get; set; }
+    public int PinNo {private get; set; }
+    public int { get; set; }
+}
+
+//Constructor 
+public CreditCard (string _creditcard, int _cardNo, int _pinNo, int _expiryDate)
+{
+    CreditCard = _creditcard;
+    CardNo = _cardNo;
+    PinNo = _pinNo;
+
+
+
+
+
+
+}
+//set the info but not get the privates
+
+
+
+      
+
+}
+//create instance
+//display card name and expiry back to user
+DateTime
